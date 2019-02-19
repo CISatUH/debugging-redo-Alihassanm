14fcf5628e01ba176add98db3d59c9ac87c39504
Alihassan <alihassanm01@gmail.com>
2019-02-18 21:39:32 -0600

    task 5, task 6, and task 10 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_200553) do
+ActiveRecord::Schema.define(version: 2019_02_19_031655) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.integer "price"
   end
 
   create_table "tasks", force: :cascade do |t|
